f6c9f1ded4a465471c8e4fdfdcac9c9a9eabab17
Keyaku <antonio.luis.sarmento@gmail.com>
2016-06-29 15:31:25 +0100

    Introducing AudioRoom pt.2
    Freeing the parent of a Stream after passing said Stream to the
    AudioRoom is a *really bad* idea.
    This fixes that.

--- GAME/SCRIPTS/AudioRoom.gd
+++ GAME/SCRIPTS/AudioRoom.gd
@@ -1,10 +1,17 @@
 extends StreamPlayer
 
+var audio_placeholder
+
 # Core functions
 func _ready():
 	set_loop(true)
 
 # Methods
 func load_music(node):
-	set_stream(node.get_stream())
+	# Free old one, if any
+	if audio_placeholder != null:
+		audio_placeholder.free()
+
+	audio_placeholder = node.duplicate()
+	set_stream(audio_placeholder.get_stream())
 	return
