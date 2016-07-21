0b0bdfa649d9cf225ab205d04596cf87885fe529
GovanifY <govanify@protonmail.com>
2016-07-21 23:00:14 +0200

    Continuing

--- GAME/SCRIPTS/Battle/Battle_EnemyHP.gd
+++ GAME/SCRIPTS/Battle/Battle_EnemyHP.gd
@@ -1,24 +1,31 @@
 extends CanvasLayer
 
-######################
-### Core functions ###
-######################
-
 var oldHP = 100
+var maxHP = 100
 onready var GreenBar = get_node("GreenBar")
 onready var SlideHit = get_node("SlideHit")
 
+######################
+### Core functions ###
+######################
+func _ready():
+	#set maxHP to Boss HP here
+	pass	
 func _set_HP(curHP):
 	GreenBar.set_scale(Vector2(curHP/100,0))
 
 func update(curHP):
-	while curHP > 100:
+	var curLine = curHP
+	while curLine > 100:
 		#TODO: Update for bubbles here
-		curHP = curHP - 100
-	_set_HP(curHP)
-	if(curHP > oldHP):
+		curLine = curHP - 100
+	_set_HP(curLine)
+	if(curLine > oldHP):
 		#Happens in debug or if heal or during one bubble shot
 		#SlideHit is 2secs long so *2
 		SlideHit.seek((curHP/100)*2)
+		#TODO: Update the HP background in case maxHP%100!=0
+		#and maxHP - curHP =< maxHP%100
 	#TODO: Add Timer for life drawing anim and play SlideHit!
-	oldHP = curHP
+	oldHP = curLine
+
