4af34818a729b3a91e62dbe0ba62d977dca9dce3
GovanifY <govanify@protonmail.com>
2016-07-21 22:23:01 +0200

    Let's continue(Ennemy HUD)

--- GAME/SCRIPTS/Battle/Battle_EnemyHP.gd
+++ GAME/SCRIPTS/Battle/Battle_EnemyHP.gd
@@ -3,6 +3,22 @@ extends CanvasLayer
 ######################
 ### Core functions ###
 ######################
-func _set_HP(HP):
+
+var oldHP = 100
+var GreenBar = get_node("GreenBar")
+var SlideHit = get_node("SlideHit")
+
+func _set_HP(curHP):
 	var GreenBar = get_node("GreenBar")
-	Greenbar.set_scale(Vector2(HP/100,0)
+	Greenbar.set_scale(Vector2(curHP/100,0)
+
+func update():
+	while curHP > 100:
+		#TODO: Update for bubbles here
+		curHP = curHP - 100
+	_set_HP(curHP)
+	if(curHP > oldHP):
+		#Happens in debug or if heal or during one bubble shot
+		#SlideHit is 2secs long so *2
+		SlideHit.seek((curHP/100)*2)
+	#TODO: Add Timer for life drawing anim and play SlideHit!
