937d88b5e9d12792b9d49d4cb0cefc8cd49b1ace
GovanifY <govanify@protonmail.com>
2016-07-21 22:03:08 +0200

    Let the fun begin! (Scripting Ennemy HUD)

--- GAME/SCRIPTS/Battle/Battle_EnemyHP.gd
+++ GAME/SCRIPTS/Battle/Battle_EnemyHP.gd
@@ -1,1 +1,8 @@
 extends CanvasLayer
+
+######################
+### Core functions ###
+######################
+func _set_HP(HP):
+	var GreenBar = get_node("GreenBar")
+	Greenbar.set_scale(Vector2(HP/100,0)
