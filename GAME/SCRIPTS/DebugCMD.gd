3791523f9aeb77e2cf9d996916191459ef149420
GovanifY <govanify@protonmail.com>
2016-07-23 14:24:43 +0200

    Adding DebugCMD

---
+++ GAME/SCRIPTS/DebugCMD.gd
@@ -0,0 +1,18 @@
+
+extends Node2D
+
+
+func _ready():
+	pass
+
+func execute(input):
+	#Basic thingy to return, have to be highly modded
+    var script = GDScript.new()
+    script.set_source_code("func code():\n\treturn " + input)
+    script.reload()
+
+    var obj = Reference.new()
+    obj.set_script(script)
+
+    return obj.code()
+
