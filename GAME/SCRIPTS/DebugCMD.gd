e577177235a23835c8d6b9527990563deeb372d9
Keyaku <antonio.luis.sarmento@gmail.com>
2016-07-30 05:35:39 +0100

    Inputting commands via DebugCMD pt.2
    Changes:
    - Fixed Panel's position and opacity
    - Better control of Return events
    - Added a history list (controlled with Up or Down)

--- GAME/SCRIPTS/DebugCMD.gd
+++ GAME/SCRIPTS/DebugCMD.gd
@@ -1,19 +1,25 @@
-extends Panel
+extends CanvasLayer
 
 # Important Nodes
-onready var CommandBox = get_node("CommandBox")
-onready var HistoryBox = get_node("HistoryBox")
+onready var CommandBox = get_node("Panel/CommandBox")
+onready var HistoryBox = get_node("Panel/HistoryBox")
 
 # Instance members
-var InputProcessing = []
+var HistoryList = {
+	"idx"  : -1,
+	"cmds" : []
+}
 
 ######################
-###    Functions   ###
+### Core functions ###
 ######################
+func _ready():
+	CommandBox.set_wrap(false)
+
+# Executes a small script with the given input
 func _execute(input):
-	#Basic thingy to return, have to be highly modded
 	var script = GDScript.new()
-	script.set_source_code("func run():\n\treturn " + input)
+	script.set_source_code("func run():\n\treturn " + str(input) + "\n")
 	script.reload()
 
 	var obj = Reference.new()
@@ -21,13 +27,53 @@ func _execute(input):
 
 	return obj.run()
 
-func _on_TextEdit_text_changed():
-	var msg = CommandBox.get_text()
-	if msg.empty():
-		return
-	if msg.find("\n") != -1:
-		_update_history("> " + str(_execute(msg))+ "\n")
-		CommandBox.set_text("")
+func _command_process():
+	var cmd = CommandBox.get_text()
+	cmd = _clean_str(cmd)
 
-func _update_history(result):
-	HistoryBox.add_text(result)
+	# Executing and saving to History
+	HistoryBox.add_text("> " + str(_execute(cmd)) + "\n")
+	if not (cmd in HistoryList.cmds):
+		HistoryList.cmds.push_back(cmd)
+		HistoryList.idx = HistoryList.cmds.size() - 1
+
+	CommandBox.clear_undo_history()
+	CommandBox.set_text("")
+
+func _clean_str(string):
+	string = string.strip_edges()
+	string = string.replace("\n", "")
+	return string
+
+func _point_to_end(idx):
+	#CommandBox.cursor_set_line(CommandBox.get_line_count())
+	CommandBox.cursor_set_column(idx)
+
+#######################
+### Signal routines ###
+#######################
+func _CommandBox_input(event):
+	var size = HistoryList.cmds.size()
+
+	if event.is_pressed() && !event.is_echo():
+		# Controlling Returns
+		if event.is_action("ui_accept"):
+			_command_process()
+			return
+
+		# Controlling HistoryList
+		var up = event.is_action("ui_up")
+		var down = event.is_action("ui_down")
+
+		if up || down:
+			if 0 <= HistoryList.idx && HistoryList.idx < size:
+				var text = HistoryList.cmds[HistoryList.idx]
+				CommandBox.set_text(text)
+				_point_to_end(text.length())
+			else:
+				CommandBox.set_text("")
+
+		if up && HistoryList.idx > 0:
+			HistoryList.idx -= 1
+		elif down && HistoryList.idx < size - 1:
+			HistoryList.idx += 1
