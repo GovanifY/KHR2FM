30c5311ea9a7605f5a6565afde1b538238c38575
Keyaku <antonio.luis.sarmento@gmail.com>
2016-08-10 01:14:34 +0100

    Rough sketch of what a cutscene SHOULD be
    But since Godot is severely lacking, AnimationTreePlayer is ultimately
    lacking and EXTREMELY buggy, this is mostly going to be scratched.

--- GAME/SCRIPTS/Game/Intro/Intro.gd
+++ GAME/SCRIPTS/Game/Intro/Intro.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 # Instance members
+onready var Sequences = get_node("Sequences")
+onready var Dialogue = get_node("Dialogue")
+var seq_cursor = 0
 var cursor = 0
-var Dialogue = null
 
 ######################
 ### Core functions ###
 ######################
 func _ready():
-	# Setting dialogue node
-	Dialogue = get_node("Dialogue")
-	Dialogue.connect("no_more_lines", self, "_set_cursor")
+	# Connecting nodes
+	Dialogue.connect("no_more_lines", self, "_inc_cursor")
+	get_node("Master").connect("finished", self, "_inc_cursor")
 
 	# Setting all lines of dialogue
 	Dialogue.set_context("INTRO_FATHERSON")
@@ -20,25 +22,30 @@ func _ready():
 
 	# Starting first lines
 	Dialogue.speak("", 9)
-	pass
 
-func _set_cursor():
+func _inc_cursor():
+	_fetch_sequence()
+	cursor += 1
+
+func _next_anim():
+	if !Sequences.is_active():
+		Sequences.set_active(true)
+	Sequences.transition_node_set_current("state", seq_cursor)
+	seq_cursor += 1
+
+func _fetch_sequence():
 	if cursor == 0:
-		cursor+=1
+		_next_anim()
 		Dialogue.set_side("right")
 		Dialogue.speak("Yuugure", 5)
 	elif cursor == 1:
-		cursor+=1
 		Dialogue.speak("Kiryoku", 4)
 	elif cursor == 2:
-		cursor += 1
 		Dialogue.speak("Yuugure", 2)
 	elif cursor == 3:
-		cursor += 1
+		_next_anim()
 		Dialogue.speak("Kiryoku", 7)
 	elif cursor == 4:
-		cursor +=1
 		#TODO: Add music&stuff
 		SceneLoader.add_scene("Game/Intro/Battle_Yuugure.tscn")
 		SceneLoader.load_new_scene()
-	pass
