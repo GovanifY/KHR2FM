9078e6cc62e950aae93f411f6fc92a8989f8a73d
GovanifY <govanify@protonmail.com>
2016-08-16 13:38:09 +0200

    Making base for new intro

--- GAME/SCRIPTS/Game/Intro/Intro.gd
+++ GAME/SCRIPTS/Game/Intro/Intro.gd
@@ -11,7 +11,6 @@ var cursor = 0
 func _ready():
 	# Connecting nodes
 	Dialogue.connect("no_more_lines", self, "_inc_cursor")
-
 	# Setting all lines of dialogue
 	Dialogue.set_context("INTRO_FATHERSON")
 	Dialogue.collect_lines("Yuugure", 13)
@@ -31,20 +30,22 @@ func _play_anim(name):
 
 func _fetch_sequence():
 	if cursor == 0:
-		_play_anim("Kiryoku down")
+		_play_anim("Water")
 	elif cursor == 1:
+		_play_anim("Kiryoku down")
+	elif cursor == 2:
 		Dialogue.set_side("right")
 		Dialogue.speak("Yuugure", 5)
-	elif cursor == 2:
-		Dialogue.speak("Kiryoku", 4)
 	elif cursor == 3:
-		Dialogue.speak("Yuugure", 2)
+		Dialogue.speak("Kiryoku", 4)
 	elif cursor == 4:
+		Dialogue.speak("Yuugure", 2)
+	elif cursor == 5:
 		# TODO: dismiss Dialogue's mugshots
 		_play_anim("Kiryoku vanish")
-	elif cursor == 5:
-		Dialogue.speak("Kiryoku", 7)
 	elif cursor == 6:
+		Dialogue.speak("Kiryoku", 7)
+	elif cursor == 7:
 		#TODO: Add music&stuff
 		SceneLoader.add_scene("Game/Intro/Battle_Yuugure.tscn")
 		SceneLoader.load_new_scene()
