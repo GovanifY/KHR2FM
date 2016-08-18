f3287566b7d1d3043f19a8e7c69835a750c79f23
Keyaku <antonio.luis.sarmento@gmail.com>
2016-08-18 23:46:32 +0100

    EnemyHP overhaul
    Using Sprites is one thing, another is having the hassle to create
    specific calculating functions just to set their sizes.
    By using TextureProgress instead, we'll use less divisions and have
    much more control over the objects in question (because, duh, the nodes
    inherit Control).
    
    Changes:
    - Sprite nodes ---> TextureProgress nodes
    - Rearranged script functions
    - Minor fixes

--- GAME/SCRIPTS/Battle/Enemy/EnemyHP.gd
+++ GAME/SCRIPTS/Battle/Enemy/EnemyHP.gd
@@ -1,54 +1,50 @@
 # Constants
 const SLIDE_DURATION = 0.5
 const DELAY_DURATION = 1.0
-const REGION_MAX_BUBBLES = 270
-const SIZE_BUBBLE = 15
 
 # Instance members
-onready var Slide = get_node("Slide")
-onready var GreenBar = get_node("Slide/GreenBar")
-onready var RedBar = get_node("Slide/RedBar")
-onready var FullBubbles = get_node("FullBubbles")
-
-# Specific data
-var NumBubbles
+onready var SlideAnim = get_node("SlideAnim")
+onready var Bubbles = get_node("Bubbles")
+onready var RedBar = get_node("RedBar")
 
 ######################
 ### Core functions ###
 ######################
+# XXX: Temporary code
+func _ready():
+	init(330)
+
 # FIXME: Currently uses division 2 times. Find better algorithms
 func _set_lifebar(curHP, both = false):
-	NumBubbles = curHP / 100 # 1st division
+	var num_bubbles = curHP / 100 # 1st division
 	var len_lifebar = fmod(curHP, 100)
 
-	if len_lifebar == 0 && NumBubbles > 0:
+	if len_lifebar == 0 && num_bubbles > 0:
 		len_lifebar = 100
-		NumBubbles -= 1
+		num_bubbles -= 1
 
-	_set_bubbles(NumBubbles)
-	_reset_tween()
+	_set_bubbles(num_bubbles)
+	set_value(len_lifebar)
 
-	var pos_cur = float(len_lifebar) / 100
-	if pos_cur >= RedBar.get_scale().x:
+	if len_lifebar >= RedBar.get_value():
 		both = true
 
-	var scale = Vector2(pos_cur, 1) # 2nd division
-	GreenBar.set_scale(scale)
 	if both:
-		RedBar.set_scale(scale)
+		RedBar.set_value(get_value())
 
 	return both
 
 func _set_bubbles(num):
-	var rect = FullBubbles.get_region_rect()
-	rect.pos.x = -REGION_MAX_BUBBLES + min(SIZE_BUBBLE * num, REGION_MAX_BUBBLES)
-	FullBubbles.set_region_rect(rect)
+	Bubbles.set_value(num * Bubbles.get_step())
 
-func _reset_tween():
-	Slide.reset_all()
-	Slide.remove_all()
+func reset_anim():
+	SlideAnim.reset_all()
+	SlideAnim.remove_all()
 
-	Slide.set_repeat(false)
+	SlideAnim.set_repeat(false)
+	# FIXME: Temporary bullshit values. Redoing this
+	SlideAnim.interpolate_method(RedBar, "set_value", RedBar.get_value(), get_value(), SLIDE_DURATION, Tween.TRANS_LINEAR, Tween.EASE_OUT, DELAY_DURATION)
+	SlideAnim.start()
 
 ###############
 ### Methods ###
@@ -62,6 +58,4 @@ func update(newHP):
 	var is_set = _set_lifebar(newHP)
 
 	if !is_set:
-		var starting_point = RedBar.get_scale()
-		Slide.follow_method(RedBar, "set_scale", starting_point, GreenBar, "get_scale", SLIDE_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, DELAY_DURATION)
-		Slide.start()
+		reset_anim()
