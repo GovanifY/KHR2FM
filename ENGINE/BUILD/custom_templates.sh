3d82a26a43318e77b82dd959d36cddfdb5aae8db
GovanifY <govanify@protonmail.com>
2016-09-17 11:10:41 +0200

    Checking CI once again

--- ENGINE/BUILD/custom_templates.sh
+++ ENGINE/BUILD/custom_templates.sh
@@ -10,8 +10,8 @@
 #2017, did you ever take
 #that trip to Iceland?
   
-num=`grep -n custom_binary/debug=\"\" export.cfg | head -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_binary/debug=\"\"+custom_binary/debug=\"template.x32\"+" export.cfg
-num=`grep -n custom_binary/debug=\"\" export.cfg | head -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_binary/debug=\"\"+custom_binary/debug=\"template.exe\"+" export.cfg
-num=`grep -n custom_package/debug=\"\" export.cfg | head -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_package/debug=\"\"+custom_package/debug=\"template.apk\"+" export.cfg
-num=`grep -n custom_package/debug=\"\" export.cfg | head -2 | tail -1 | awk -F: '{ print $1}'`; sed -i "${num},${num}s+custom_package/debug=\"\"+custom_package/debug=\"template.zip\"+" export.cfg
+num=`grep -n custom_binary/debug=\"\" export.cfg | head -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_binary/debug=\"\"+custom_binary/debug=\"../template.x32\"+" export.cfg
+num=`grep -n custom_binary/debug=\"\" export.cfg | head -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_binary/debug=\"\"+custom_binary/debug=\"../template.exe\"+" export.cfg
+num=`grep -n custom_package/debug=\"\" export.cfg | head -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_package/debug=\"\"+custom_package/debug=\"../template.apk\"+" export.cfg
+num=`grep -n custom_package/debug=\"\" export.cfg | head -2 | tail -1 | awk -F: '{ print $1}'`; sed -i "1,${num}s+custom_package/debug=\"\"+custom_package/debug=\"../template.zip\"+" export.cfg
  
