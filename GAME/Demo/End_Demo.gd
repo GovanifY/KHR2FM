8d440aeb9cc0defd0776f28c4974780df2302c48
Keyaku <antonio.luis.sarmento@gmail.com>
2016-10-09 02:24:36 +0100

    Minimally fixed End_Demo scene
    Fixed End_Demo sufficiently for it to run.
    Still needs to be revised.

---
+++ GAME/Demo/End_Demo.gd
@@ -0,0 +1,26 @@
+extends Node
+
+# Instance members
+onready var Dialogue = get_node("Dialogue")
+var cursor = 0
+
+######################
+### Core functions ###
+######################
+func _ready():
+	# Connecting nodes
+	Dialogue.connect("no_more_lines", self, "_inc_cursor")
+	Dialogue.Bubble.set_bubble_skin(0)
+
+	# Starting first lines
+	_fetch_sequence()
+
+func _inc_cursor():
+	cursor += 1
+	_fetch_sequence()
+
+func _fetch_sequence():
+	if cursor == 0:
+		Dialogue.speak("Dev", 0, 3)
+	else:
+		KHR2.quit_game()
