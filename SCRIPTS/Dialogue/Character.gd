1572a01cfbed6698599c590d67dba8dd978e89b9
Keyaku <antonio.luis.sarmento@gmail.com>
2016-11-14 21:17:07 +0000

    Character's name is mirrored by the node's Name

--- SCRIPTS/Dialogue/Character.gd
+++ SCRIPTS/Dialogue/Character.gd
@@ -1,37 +1,36 @@
 extends Sprite
 
 # Export values
-export(String) var name = String()
 export(int, "Character", "Narrator") var type = 0
 
 # Instance members
+onready var name = get_name()
 var begin = 0
 var end   = 0
-
-# "Private" members
-const self_type = "Character"
 
 ######################
 ### Core functions ###
 ######################
 func _ready():
-	add_to_group(self_type)
-
 	hide() # Hide it initially
 	set_pos(0) # Aligning vertically
 
 ###############
 ### Methods ###
 ###############
-func is_type(type):
-	return type == self_type
+func set_pos(pos):
+	if typeof(pos) == TYPE_INT:
+		pos = Vector2(pos, 0)
+	pos.y = int(OS.get_video_mode_size().height) >> 1
+	# We're only accepting the X value
+	.set_pos(pos)
+
+### FIXME: temporary functions
+func get_type():
+	return "Character"
+
+func is_type(istype):
+	return istype == "Character"
 
 func get_pos():
 	return .get_pos().x
-
-func set_pos(x):
-	# In case we're given a Vector2, we're only accepting the X value
-	if typeof(x) == TYPE_VECTOR2:
-		x = x.x
-
-	.set_pos(Vector2(x, int(OS.get_video_mode_size().height) >> 1))
