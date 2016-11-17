45d7908db6db4ef1210892e0792c0e26b118e557
Keyaku <antonio.luis.sarmento@gmail.com>
2016-11-17 19:24:10 +0000

    Patched Intro scene and script to work with current Dialogue

--- GAME/STORY/Intro/Intro.gd
+++ GAME/STORY/Intro/Intro.gd
@@ -1,52 +1,48 @@
-extends Node2D
+extends Node
 
 # Instance members
 onready var Dialogue = get_node("Dialogue")
 onready var Sequences = get_node("Master")
-var cursor = 0
+
+# Characters
+onready var Kiryoku  = get_node("Dialogue/Kiryoku")
+onready var Yuugure  = get_node("Dialogue/Yuugure")
 
 ######################
 ### Core functions ###
 ######################
 func _ready():
-	# Connecting nodes
-	Dialogue.connect("finished", self, "_inc_cursor")
-	# Setting all lines of dialogue
-	Dialogue.set_context("INTRO_FATHERSON")
-	Dialogue.collect_lines("Yuugure", 13)
-	Dialogue.collect_lines("Kiryoku", 20)
-
-	# Starting first lines
-	AudioRoom.load_music(get_node("Into the Darkness"))
-	AudioRoom.play()
+	# Begin cutscene
 	_fetch_sequence()
-
-func _inc_cursor():
-	cursor += 1
-	_fetch_sequence()
-
-func _play_anim(name):
-	Sequences.play(name)
-	yield(Sequences, "finished")
-	_inc_cursor()
 
 func _fetch_sequence():
-	if cursor == 0:
-		_play_anim("Water")
-	elif cursor == 1:
-		_play_anim("Kiryoku down")
-	elif cursor == 2:
-		Dialogue.set_side("right")
-		Dialogue.speak("Yuugure", 5)
-	elif cursor == 3:
-		Dialogue.speak("Kiryoku", 4)
-	elif cursor == 4:
-		Dialogue.speak("Yuugure", 2)
-	elif cursor == 5:
-		# TODO: dismiss Dialogue's mugshots
-		_play_anim("Kiryoku vanish")
-	elif cursor == 6:
-		Dialogue.speak("Kiryoku", 7)
-	elif cursor == 7:
-		#TODO: Add music&stuff
-		SceneLoader.load_scene("Intro/Battle_Yuugure.tscn")
+	# Starting music
+	AudioRoom.load_music(get_node("Into the Darkness"))
+	AudioRoom.play()
+
+	Sequences.play("Water")
+	yield(Sequences, "finished")
+
+	Sequences.play("Kiryoku down")
+	yield(Sequences, "finished")
+
+	Yuugure.set_pos(Vector2(700, 0))
+	Dialogue.speak(Yuugure, 0, 5)
+	yield(Dialogue, "finished")
+
+	Kiryoku.set_pos(Vector2(200, 0))
+	Dialogue.speak(Kiryoku, 0, 4)
+	yield(Dialogue, "finished")
+
+	Dialogue.speak(Yuugure, 5, 7)
+	yield(Dialogue, "finished")
+
+	# TODO: dismiss Dialogue's mugshots
+
+	Sequences.play("Kiryoku vanish")
+	yield(Sequences, "finished")
+
+	Dialogue.speak(Kiryoku, 4, 7)
+	yield(Dialogue, "finished")
+
+	SceneLoader.load_scene("Intro/Battle_Yuugure.tscn")
