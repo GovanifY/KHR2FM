ad87f9801979d7d08ad2f11bad8ff5140e2f5427
Gauvain Roussel-Tarbouriech <govanify@protonmail.com>
2016-11-18 14:52:56 +0100

    Maps collisions and moving before getting rekt in physics. Missing scrolling

--- SCRIPTS/Map/Player/Player.gd
+++ SCRIPTS/Map/Player/Player.gd
@@ -1,8 +1,24 @@
 extends KinematicBody2D
 
+const MOTION_SPEED = 160 # Pixels/seconds
 func _ready():
 	set_process_input(true)
+	set_fixed_process(true)
 
+func _fixed_process(delta):
+	var motion = Vector2()
+	
+	if (Input.is_action_pressed("ui_up")):
+		motion += Vector2(0, -1)
+	if (Input.is_action_pressed("ui_down")):
+		motion += Vector2(0, 1)
+	if (Input.is_action_pressed("ui_left")):
+		motion += Vector2(-1, 0)
+	if (Input.is_action_pressed("ui_right")):
+		motion += Vector2(1, 0)
+	
+	motion = motion.normalized()*MOTION_SPEED*delta
+	motion = move(motion)
 func _input(event):
 	var left  = Input.is_action_pressed("ui_left")
 	var right = Input.is_action_pressed("ui_right")
