3ef8485bafbb9d3f67c93957a29eb244aaf45122
Keyaku <antonio.luis.sarmento@gmail.com>
2017-01-23 03:20:50 +0000

    Map: Player movement code refactor
    Changes:
    - Renamed player_* —> sprite_*
    - Using bit masks for each directional Input
    - Simpler and more abstract sprite motion and animation code
    - Code cleanup

--- SCRIPTS/Map/Player.gd
+++ SCRIPTS/Map/Player.gd
@@ -3,20 +3,34 @@ extends KinematicBody2D
 # Constants
 const MOTION_SPEED = 300 # Pixels/second
 
+enum {
+	SPR_UP = 1, SPR_DOWN = 2, SPR_LEFT = 4, SPR_RIGHT = 8,
+}
+
 # Instance members
 onready var Anims     = get_node("anims")
 onready var Character = get_node("Character")
 
 # "Private" members
-onready var player_frame = {
-	"down"       : Character.hframes * 0,
-	"left"       : Character.hframes * 1,
-	"right"      : Character.hframes * 2,
-	"up"         : Character.hframes * 3,
-	"up_left"    : Character.hframes * 4,
-	"down_right" : Character.hframes * 5,
-	"down_left"  : Character.hframes * 6,
-	"up_right"   : Character.hframes * 7,
+onready var sprite_direction = { # All 8 directions from this project's spriteset layout
+	SPR_UP               : "up"        ,
+	SPR_DOWN             : "down"      ,
+	SPR_LEFT             : "left"      ,
+	SPR_RIGHT            : "right"     ,
+	SPR_UP   | SPR_LEFT  : "up_left"   ,
+	SPR_UP   | SPR_RIGHT : "up_right"  ,
+	SPR_DOWN | SPR_LEFT  : "down_left" ,
+	SPR_DOWN | SPR_RIGHT : "down_right",
+}
+onready var sprite_motion = {
+	SPR_UP               : Vector2( 0, -1),
+	SPR_DOWN             : Vector2( 0,  1),
+	SPR_LEFT             : Vector2(-1,  0),
+	SPR_RIGHT            : Vector2( 1,  0),
+	SPR_UP   | SPR_LEFT  : Vector2(-1, -1),
+	SPR_UP   | SPR_RIGHT : Vector2( 1, -1),
+	SPR_DOWN | SPR_LEFT  : Vector2(-1,  1),
+	SPR_DOWN | SPR_RIGHT : Vector2( 1,  1),
 }
 
 ######################
@@ -25,75 +39,39 @@ onready var player_frame = {
 func _ready():
 	set_fixed_process(true)
 
-# Totally stolen from isometric example but eh
 func _fixed_process(delta):
-	# Grabbing Input
-	var left  = Input.is_action_pressed("left")
-	var right = Input.is_action_pressed("right")
-	var up    = Input.is_action_pressed("up")
-	var down  = Input.is_action_pressed("down")
+	# Grabbing directions from Input and transforming them into flags
+	var directions = int(Input.is_action_pressed("up"))    << 0
+	directions    |= int(Input.is_action_pressed("down"))  << 1
+	directions    |= int(Input.is_action_pressed("left"))  << 2
+	directions    |= int(Input.is_action_pressed("right")) << 3
 
-	animate_character(up, down, left, right)
-	move_character(up, down, left, right, delta)
-
-func _input(event):
-	pass
+	# If it can animate the character, then it can move
+	if animate_character(directions):
+		move_character(directions, delta)
 
 ###############
 ### Methods ###
 ###############
-func animate_character(up, down, left, right):
-	var current_anim = Anims.get_current_animation()
-	var new_anim
-
-	# Order is key: first, the multiple inputs; then, the single ones
-	if up && right:
-		new_anim = "up_right"
-	elif up && left:
-		new_anim = "up_left"
-	elif down && right:
-		new_anim = "down_right"
-	elif down && left:
-		new_anim = "down_left"
-	elif up:
-		new_anim = "up"
-	elif down:
-		new_anim = "down"
-	elif left:
-		new_anim = "left"
-	elif right:
-		new_anim = "right"
-
-	# If not moving at all or going separate ways
-	var opposite_ways = (left && right) || (up && down)
-
-	if not (up || down || left || right) || opposite_ways:
-		Anims.stop()
-		if player_frame.has(current_anim):
-			Character.set_frame(player_frame[current_anim])
-	else:
-		if current_anim != new_anim || !Anims.is_playing():
-			current_anim = new_anim
+func animate_character(directions):
+	# If it can move to this direction
+	if sprite_direction.has(directions):
+		var new_anim = sprite_direction[directions]
+		if new_anim != Anims.get_current_animation() || !Anims.is_playing():
 			Anims.play(new_anim)
+	else:
+		Anims.stop()
+		var idle_frame = Character.get_frame() - (Character.get_frame() % Character.hframes)
+		Character.set_frame(idle_frame)
 
-func move_character(up, down, left, right, delta=0):
-	var motion = Vector2()
+	return Anims.is_playing()
 
-	if left && !right:
-		motion += Vector2(-1, 0)
-	elif !left && right:
-		motion += Vector2(1, 0)
-
-	if up && !down:
-		motion += Vector2(0, -1)
-	elif !up && down:
-		motion += Vector2(0, 1)
-
+func move_character(directions, delta=0):
+	var motion = sprite_motion[directions]
 	motion = motion.normalized() * MOTION_SPEED * delta
 	motion = move(motion)
 
-	# Make character slide nicely through the world
-	# tl;dr slides when collisions detected
+	# Make character slide when collisions are detected
 	var slide_attempts = 4
 	while is_colliding() and slide_attempts > 0:
 		motion = get_collision_normal().slide(motion)
