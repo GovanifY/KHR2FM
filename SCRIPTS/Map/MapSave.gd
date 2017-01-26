3589aa15a700999f76d31dec4f91d12cb7b77f0f
Gauvain Roussel-Tarbouriech <govanify@protonmail.com>
2017-01-26 08:57:48 +0100

    Save points, somewhat usable

--- SCRIPTS/Map/MapSave.gd
+++ SCRIPTS/Map/MapSave.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends StaticBody2D
 
 # class member variables go here, for example:
 # var a = 2
@@ -8,3 +8,22 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+#######################
+### Signal routines ###
+#######################
+func _on_TalkArea_body_enter( body ):
+	if body.get_name() == "Player":
+		set_process_input(true)
+		get_node("approach").play("approach")
+	
+func _on_TalkArea_body_exit( body ):
+	if body.get_name() == "Player":
+		set_process_input(false)
+		
+###############
+### Methods ###
+###############
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		print("We should pop up save menu here")
