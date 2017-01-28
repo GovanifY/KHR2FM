909c2cdbab4a445a7542f3f94655072c4e1c1238
Keyaku <antonio.luis.sarmento@gmail.com>
2017-01-28 02:28:32 +0000

    Arranged Intro cutscene's dialogue
    Still misses all the flashy stuff, and suffers from Dialogue’s bugs and
    oddities.

--- GAME/STORY/Intro/Intro.gd
+++ GAME/STORY/Intro/Intro.gd
@@ -21,32 +21,46 @@ func _ready():
 	AudioRoom.play()
 
 	# Begin cutscene
-	_fetch_sequence()
-
-func _fetch_sequence():
 	Sequences.play("Water")
 	yield(Sequences, "finished")
 
 	Sequences.play("Kiryoku down")
 	yield(Sequences, "finished")
 
-	Dialogue.speak(Yuugure, 0, 1)
+	Dialogue.speak(Yuugure, 0, 3)
 	yield(Dialogue, "finished")
 
-	Dialogue.speak(Kiryoku, 0, 4)
+	Dialogue.speak(Kiryoku, 0, 3)
 	yield(Dialogue, "finished")
 
-	Dialogue.speak(Yuugure, 5, 7)
+	Dialogue.speak(Yuugure, 4, 6)
 	yield(Dialogue, "finished")
 
 	Dialogue.silence()
 	yield(Dialogue, "finished")
 
+	Dialogue.silence()
 	Sequences.play("Kiryoku vanish")
 	yield(Sequences, "finished")
 
-	Dialogue.speak(Kiryoku, 4, 7)
+	# Change music
+	AudioRoom.load_music(get_node("The Eye of the Darkness"))
+	AudioRoom.play()
+
+	Dialogue.speak(Kiryoku, 4, 10)
 	yield(Dialogue, "finished")
 
-	Dialogue.silence()
+	Dialogue.speak(Yuugure, 7, 8)
+	yield(Dialogue, "finished")
+
+	Dialogue.speak(Kiryoku, 11, 16)
+	yield(Dialogue, "finished")
+
+	Dialogue.speak(Yuugure, 9, 12)
+	yield(Dialogue, "finished")
+
+	Dialogue.speak(Kiryoku, 17, 19)
+	yield(Dialogue, "finished")
+
+	# Load next scene
 	SceneLoader.load_scene("STORY/Intro/Battle_Yuugure.tscn")
