e57c05093a371bf35879b6e56df509b6ebd4ee2f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-01-28 02:35:06 +0000

    AudioRoom: Code cleanup
    StreamPlayer creates an instance for us, so no need to duplicate it
    ourselves.

--- SCRIPTS/AudioRoom.gd
+++ SCRIPTS/AudioRoom.gd
@@ -1,18 +1,11 @@
 extends StreamPlayer
 
-var current_audio
-
 # Core functions
 func _ready():
 	set_loop(true)
 
 # Methods
 func load_music(node):
-	# Free old one, if any
-	if current_audio != null:
-		current_audio.stop()
-		current_audio.free()
-
-	current_audio = node.duplicate()
-	set_stream(current_audio.get_stream())
+	stop()
+	set_stream(node.get_stream())
 	return
