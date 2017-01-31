c0bc8fd23df44f772fd04d5d662de219b759aef7
Keyaku <antonio.luis.sarmento@gmail.com>
2017-01-31 10:49:53 +0000

    Dialogue: Avatar animations improvements
    Changes:
    - Specified cases of Tween behaviors
    - Displaying and dismissing Avatars now both fade and drag
    - Fixed Hook positioning issues
    - Fixed graphical oddities
    - Some code refactoring

--- SCRIPTS/Dialogue/Dialogue.gd
+++ SCRIPTS/Dialogue/Dialogue.gd
@@ -65,40 +65,51 @@ func _close_dialogue():
 	# Resetting values
 	index = -1
 	current_speaker = null
+	Bubble.Hook.hide()
 
 	set_process_input(false)
 	emit_signal("finished")
+
+func _center_hook():
+	var center = current_speaker.get_center()
+	if current_speaker.is_flipped_h():
+		center += CastRight.get_pos().x
+	Bubble.set_hook(current_speaker, center)
 
 #######################
 ### Signal routines ###
 #######################
 func _on_CastAnim_tween_complete(object, key):
-	# If the key was about setting offset, it's about speak()
 	if key == "set_offset":
-		var character = object.get_parent()
-		# Centering hook (whenever necessary)
-		var center = character.get_center()
-		if CastRight.is_a_parent_of(character):
-			center += CastRight.get_pos().x
-		Bubble.set_hook(character, center)
-
-		if Bubble.is_hidden():
-			Bubble.show_box()
-		else:
-			Bubble.emit_signal("shown")
-		return
-	# Otherwise, it's about setting opacity
+		# If object's offset is 0, it's been displayed.
+		# FIXME: I have to round this value because Godot is acting like JavaScript
+		# and returning me values like -0.000031!!!
+		var rounded_offset = round(object.get_offset().x)
+		if rounded_offset == 0:
+			_center_hook()
+			if Bubble.is_hidden():
+				Bubble.show_box()
+			else:
+				Bubble.emit_signal("shown")
 	elif key == "set_opacity":
-		object.hide()
-		object.set_opacity(1.0)
-	emit_signal("finished")
+		# If object's opacity is 0, it's been dismissed
+		if object.get_opacity() == 0:
+			object.hide()
+			object.set_opacity(1.0)
+
+			emit_signal("finished")
+		# Otherwise, it's being displayed
+		else:
+			object.show()
 
 func _get_line():
 	# Parsing lineID
-	var lineID = ""
-	if !current_speaker.get_name().empty():
-		lineID += current_speaker.get_name().to_upper() + "_"
+	var lineID = current_speaker.get_name().to_upper() + "_"
 	lineID += "%02d" % index
+
+	# Centering hook
+	if Bubble.Hook.is_hidden():
+		_center_hook()
 
 	# Incrementing index
 	index += 1
@@ -164,23 +175,32 @@ func silence():
 func display(character):
 	var off_bounds = character.Avatar.get_texture().get_size()
 	off_bounds.y = 0
+	# Drag animation from left or right depending on the situation
 	if !character.is_flipped_h():
 		off_bounds.x *= -1
 
-	# Drag animation from left or right depending on the situation
 	CastAnim.interpolate_method(character.Avatar, "set_offset", off_bounds, Vector2(), ANIM_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	CastAnim.interpolate_method(character, "set_opacity", 0.0, 1.0, 0.01, Tween.TRANS_LINEAR, Tween.EASE_IN)
 
 	CastAnim.start()
-	character.show()
 
 # Dismisses a specified Avatar, or all of them if NULL
 func dismiss(character=null):
+	var to_dismiss = Array()
 	if character != null:
-		CastAnim.interpolate_method(character, "set_opacity", 1.0, 0.0, ANIM_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		to_dismiss.push_back(character)
 	else:
-		var to_dismiss = CastLeft.get_children()
+		to_dismiss = CastLeft.get_children()
 		to_dismiss += CastRight.get_children()
-		for avatar in to_dismiss:
-			CastAnim.interpolate_method(avatar, "set_opacity", 1.0, 0.0, ANIM_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
+
+	for character in to_dismiss:
+		var off_bounds = character.Avatar.get_texture().get_size()
+		off_bounds.y = 0
+		# Drag animation from left or right depending on the situation
+		if !character.is_flipped_h():
+			off_bounds.x *= -1
+
+		CastAnim.interpolate_method(character.Avatar, "set_offset", Vector2(), off_bounds, ANIM_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		CastAnim.interpolate_method(character, "set_opacity", 1.0, 0.0, ANIM_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
 
 	CastAnim.start()
