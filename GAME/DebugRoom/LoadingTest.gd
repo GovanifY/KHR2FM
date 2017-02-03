b406479fcac304592976aab18e1d6dcb09d096f9
Keyaku <antonio.luis.sarmento@gmail.com>
2017-02-03 15:50:06 +0000

    SceneLoader: Refactored tests
    I’m letting the code decide the number of buttons to create. Results in
    a much simpler environment

--- GAME/DebugRoom/LoadingTest.gd
+++ GAME/DebugRoom/LoadingTest.gd
@@ -9,6 +9,7 @@ var background_tooltips = [
 	"Loads in the background, then erases it (shows nothing)",
 	"Loads in the background, then shows it on top of the current scene",
 	"Loads in the background, then switches to it",
+	"Loads 2 scenes in the background, then switches to the first"
 ]
 var foreground_tooltips = [
 	"Loads in the foreground.",
@@ -30,11 +31,11 @@ func _ready():
 	Background.connect("button_selected", self, "_on_background_press")
 	Foreground.connect("button_selected", self, "_on_foreground_press")
 
-	# Setting tooltips (I prefer doing this by code)
-	for i in range(Background.get_button_count()):
-		Background.set_button_tooltip(i, background_tooltips[i])
-	for i in range(Foreground.get_button_count()):
-		Foreground.set_button_tooltip(i, foreground_tooltips[i])
+	# Setting buttons & tooltips (I prefer doing this by code)
+	for i in range(background_tooltips.size()):
+		Background.add_button(String(i), background_tooltips[i])
+	for i in range(foreground_tooltips.size()):
+		Foreground.add_button(String(i), foreground_tooltips[i])
 
 func _on_background_press(button_idx):
 	var method = "background" + String(button_idx)
@@ -70,6 +71,11 @@ func background1():
 func background2():
 	SceneLoader.load_scene(path, true)
 	SceneLoader.show_scene(path, true)
+
+func background3():
+	SceneLoader.load_scene("DebugRoom/DialogueTest.tscn", true)
+	SceneLoader.load_scene("STORY/Intro/Battle_Yuugure.tscn", true)
+	SceneLoader.show_scene("DebugRoom/DialogueTest.tscn", true)
 
 # Foreground tests
 func foreground0():
