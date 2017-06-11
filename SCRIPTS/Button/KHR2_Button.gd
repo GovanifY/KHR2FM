9dfeb58e032b010bfbed1e308b6697f7986b7a4f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-11 18:35:59 +0100

    Button: Don't play SE when forcibly grabbing focus

--- SCRIPTS/Button/KHR2_Button.gd
+++ SCRIPTS/Button/KHR2_Button.gd
@@ -1,3 +1,10 @@
+extends Button
+
 func _ready():
 	connect("pressed", SE, "play", ["system_confirm"])
+	connect("focus_enter", SE, "play", ["system_selected"])
+
+func grab_focus():
+	disconnect("focus_enter", SE, "play")
+	.grab_focus()
 	connect("focus_enter", SE, "play", ["system_selected"])
