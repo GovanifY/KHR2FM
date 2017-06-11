d31a8153acd983c1b29b5cd425427ebc367277e5
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-11 17:38:54 +0100

    TItle: Reinstated button icon when selected/focused on

--- GAME/Splash/TitleOption.gd
+++ GAME/Splash/TitleOption.gd
@@ -1,12 +1,8 @@
-extends Button
+extends "res://SCRIPTS/Button/KHR2_Button.gd"
 
-# Button icons
 const TITLE_BUTTON_NORMAL   = preload("res://ASSETS/GFX/Title/MainMenu/TitleOption_icon_normal.png")
 const TITLE_BUTTON_SELECTED = preload("res://ASSETS/GFX/Title/MainMenu/TitleOption_icon_selected.png")
 
-######################
-### Core functions ###
-######################
 func _ready():
 	connect("focus_enter", self, "set_button_icon", [TITLE_BUTTON_SELECTED])
 	connect("focus_exit", self, "set_button_icon", [TITLE_BUTTON_NORMAL])
