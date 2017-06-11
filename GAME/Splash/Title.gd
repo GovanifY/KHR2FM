71a388ec5a03a8e6b26a420b3c95bab1acb81746
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-11 01:43:03 +0100

    Title: Added Load screen to Title
    Changes:
    - Added specific animation for Loading a game (it’s just a fade, though)
    - Implemented load functionality thanks to SaveManager

--- GAME/Splash/Title.gd
+++ GAME/Splash/Title.gd
@@ -9,8 +9,9 @@ onready var Options   = get_node("Options")
 onready var Cursor    = get_node("Options/Cursor")
 onready var cursor_inc = Cursor.get_texture().get_size() / 4
 
-# New game instance members
-onready var NewGame = get_node("BG_New")
+# Options instance members
+onready var NewGame  = get_node("BG_New")
+onready var LoadGame = get_node("BG_Load")
 
 ######################
 ### Core functions ###
@@ -25,6 +26,10 @@ func _ready():
 	# Connecting New Game options
 	NewGame.connect("dismiss", NewGame.anims, "play", ["Fade Out"])
 	NewGame.connect("finished", self, "_start_new")
+
+	# Connecting New Game options
+	LoadGame.connect("dismiss", LoadGame.anims, "play", ["Fade Out"])
+	LoadGame.connect("finished", self, "_start_load")
 
 	# Adding music
 	AudioRoom.set_stream(preload("res://ASSETS/BGM/Dearly_Beloved.ogg"))
@@ -53,7 +58,7 @@ func _pressed_main(button_idx):
 	if button_idx == OPTION_MAIN_NEW:
 		NewGame.anims.play("Fade In")
 	elif button_idx == OPTION_MAIN_LOAD:
-		pass # TODO: show save slots
+		LoadGame.anims.play("Fade In")
 	elif button_idx == OPTION_MAIN_QUIT:
 		AnimsMenu.connect("finished", get_tree(), "quit")
 		AnimsMenu.play("Close")
@@ -62,10 +67,18 @@ func _start_new():
 	# Dismiss the window before anything else
 	NewGame.anims.play("Fade Out")
 	yield(NewGame.anims, "finished")
+
 	# Load Aqua intro at the end of the flashy animation
 	AudioRoom.fade_out(1)
 	AnimsMenu.connect("finished", SceneLoader, "load_scene", ["res://GAME/STORY/Intro/Aqua.tscn"])
 	AnimsMenu.play("New Game")
 
 func _start_load():
-	pass # TODO: selected save slot
+	# Dismiss the window before anything else
+	LoadGame.anims.play("Fade Out")
+	yield(LoadGame.anims, "finished")
+
+	# Load scene now updated in SaveManager
+	AudioRoom.fade_out(1)
+	AnimsMenu.connect("finished", SceneLoader, "load_scene", [SaveManager.get_scene()])
+	AnimsMenu.play("Load Game")
