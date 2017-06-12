c98d12a5aa4320e41a0597b43af2bc301b16fad7
Gauvain Roussel-Tarbouriech <govanify@protonmail.com>
2017-06-12 17:07:53 +0200

    Basic anims

--- SCRIPTS/Battle/Enemy/Enemy.gd
+++ SCRIPTS/Battle/Enemy/Enemy.gd
@@ -17,3 +17,10 @@ func get_type():
 
 func is_type(type):
 	return type == get_type()
+
+### Handling animations
+func random_voice(snd_arr):
+	var voice = get_node("Voice")
+	if typeof(snd_arr) == TYPE_STRING_ARRAY && voice != null:
+		var rng = randi() % snd_arr.size()
+		voice.play(snd_arr[rng])
