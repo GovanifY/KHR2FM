3ef405ccda609812a5c1878ef4f36c51a7c6d00d
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-16 05:27:33 +0100

    MapEvent: Even more simplification
    Forget attaching scripts or nodes, MapEvent now works with signals.
    Cleaner and simpler to use.
    
    Changes:
    - Removed exported values concerning MapEvent behavior
    - Added `touched` and `interacted` signals

--- addons/map_event/MapEvent.gd
+++ addons/map_event/MapEvent.gd
@@ -1,14 +1,16 @@
 tool
 extends Area2D
 
+# Signals
+signal touched
+signal interacted
+
+# Spriteset values
 export(Texture) var spriteset setget set_spriteset
 export(int) var hframes = 1 setget set_hframes
 export(int) var vframes = 1 setget set_vframes
 export(int) var frame = 0   setget set_frame
 export(int, "Mix", "Add", "Sub", "Mul", "PMAlpha") var blend_mode = 0 setget set_blend_mode
-export(GDScript) var behavior
-export(NodePath) var touch_node = NodePath()
-export(NodePath) var interact_node = NodePath()
 
 # Instance members
 var Character = Sprite.new()
@@ -55,19 +57,16 @@ func _enter_tree():
 func _input(event):
 	if event.is_pressed() && !event.is_echo():
 		if event.is_action("ui_accept"):
-			behavior.interact(get_node(interact_node))
+			emit_signal("interacted")
 
 #######################
 ### Signal routines ###
 #######################
 func _on_area_body_enter(body):
-	if body.get_type() == "MapPlayer":
-		if !interact_node.is_empty():
-			set_process_input(true)
-		if !touch_node.is_empty():
-			behavior.touch(get_node(touch_node))
+	if body.get_type() == "MapPlayer" && !is_processing_input():
+		set_process_input(true)
+		emit_signal("touched")
 
 func _on_area_body_exit(body):
 	if body.get_type() == "MapPlayer":
-		if !interact_node.is_empty():
-			set_process_input(false)
+		set_process_input(false)
