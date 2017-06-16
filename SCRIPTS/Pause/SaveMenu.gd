51e027c761409a5d0aa752c21b1d306344bc288f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-17 00:54:31 +0100

    SaveMenu: First draft of a save menu
    Changes:
    - Extending LoadMenu’s structure
    - Added ability to save over a new file
    - Set SavePoint as a pause menu (could be, but it’s a bit different
    when a player just wants to save at a MapEvent)

---
+++ SCRIPTS/Pause/SaveMenu.gd
@@ -0,0 +1,41 @@
+extends "LoadMenu.gd"
+
+######################
+### Core functions ###
+######################
+func _ready():
+	connect("finished", self, "_refresh")
+
+#######################
+### Signal routines ###
+#######################
+# When this window is shown
+func _show():
+	# Adding "New save" slot before loading all the others
+	var new_save = List.new_slot(self, "_pressed")
+	new_save.set_name("-1")
+	._show()
+
+func _refresh():
+	List.cleanup()
+	_show()
+
+func _pressed(button):
+	var slot_idx = int(button.get_name())
+
+	# Find the appropriate slot_idx
+	if slot_idx < 0:
+		slot_idx = SaveManager.find_available_slot()
+	else:
+		return # FIXME: Remove this
+		Info.msg.set_text("MENU_SAVE_CONFIRM")
+		anims.play("Show Info")
+		yield(anims, "finished")
+
+		return # TODO: Ask player for confirmation
+
+	SaveManager.connect("saved", self, "_done")
+
+	Info.msg.set_text("MENU_SAVE_WAIT")
+	anims.play("Show Info")
+	SaveManager.save_game(slot_idx)
---
+++ SCRIPTS/Pause/SavePoint.gd
@@ -0,0 +1,67 @@
+extends Panel
+
+# Constants
+enum { SAVEPOINT_SAVE, SAVEPOINT_WORLD }
+
+# Instance members
+onready var Options  = get_node("Options").get_children()
+onready var SaveMenu = get_node("SaveMenu")
+
+######################
+### Core functions ###
+######################
+func _enter_tree():
+	Globals.set("Pause", "SavePoint")
+	KHR2.connect("toggle_pause", self, "show")
+
+func _exit_tree():
+	Globals.set("Pause", null)
+
+func _ready():
+	connect("draw", self, "_show")
+
+	# Connecting main options
+	for i in range(0, Options.size()):
+		Options[i].connect("pressed", self, "_pressed", [i])
+
+func _input(event):
+	if event.is_pressed() && !event.is_echo():
+		if event.is_action("ui_cancel"):
+			if !SaveMenu.is_hidden():
+				_dismiss_save_menu()
+			# TODO: Add condition for if the confirmation window is visible
+			else:
+				_dismiss_menu()
+
+func _dismiss_menu():
+	set_process_input(false)
+	hide()
+
+func _dismiss_save_menu():
+	if !SaveMenu.anims.is_playing():
+		for i in range(0, Options.size()):
+			Options[i].set_focus_mode(FOCUS_ALL)
+		Options[0].grab_focus()
+
+		SaveMenu.anims.play("Fade Out")
+
+#######################
+### Signal routines ###
+#######################
+func _show():
+	Options[0].grab_focus()
+	set_process_input(true)
+
+func _pressed(button_idx):
+	# General rules
+	for i in range(0, Options.size()):
+		Options[i].set_focus_mode(FOCUS_NONE)
+
+	# Specific rules
+	if button_idx == SAVEPOINT_SAVE:
+		if SaveMenu.anims.is_playing():
+			yield(SaveMenu.anims, "finished")
+		SaveMenu.anims.play("Fade In")
+
+	elif button_idx == SAVEPOINT_WORLD:
+		pass # TODO: Ask if player wants to access the world map
