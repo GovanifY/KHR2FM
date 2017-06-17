85bda2a71238269f1f3cfa37dbb15ab487e13a0e
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-17 18:15:25 +0100

    MapPlan: Activated possibility for pausing process
    Yep, once again, I forgot something.

--- SCRIPTS/Map/MapPlan.gd
+++ SCRIPTS/Map/MapPlan.gd
@@ -4,5 +4,6 @@ export(String) var world_name = ""
 export(String) var location = ""
 
 func _ready():
+	Globals.set("Pause", "world")
 	Globals.set("World", world_name)
 	Globals.set("Location", location)
