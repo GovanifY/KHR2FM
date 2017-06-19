0da48569f8ac809664fc463c81af7ec9b266eb40
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-19 17:51:28 +0100

    MapEvent: Explicity
    Changes:
    - Send which body touched the MapEvent when emitting signal
    - Minor code change

--- addons/map_event/MapEvent.gd
+++ addons/map_event/MapEvent.gd
@@ -18,12 +18,12 @@ func _enter_tree():
 ### Signal routines ###
 #######################
 func _on_area_body_enter(body):
-	emit_signal("touched")
-	if body.get_type() == "MapPlayer":
+	emit_signal("touched", body)
+	if body.is_type("MapPlayer"):
 		body.interactable = self
 		body.set_process_input(true)
 
 func _on_area_body_exit(body):
-	if body.get_type() == "MapPlayer":
+	if body.is_type("MapPlayer"):
 		body.interactable = null
 		body.set_process_input(false)
