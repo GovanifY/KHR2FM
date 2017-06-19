bd5539b568a9a78d2d091859fc9600d4af2a68a7
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-19 17:46:32 +0100

    MapEvent: Removed embedded sprite
    Although the intention was good, it lacked flexibility to edit the
    sprite as much as the designer would want.
    Plus, it’s unnecessary, since many MapEvents won’t need a sprite at all.
    
    Changes:
    - Removed embedded sprite
    - Changed all affected MapEvents
    - Applied compressed lossless to a sprite

--- addons/map_event/MapEvent.gd
+++ addons/map_event/MapEvent.gd
@@ -5,50 +5,10 @@ extends Area2D
 signal touched
 signal interacted
 
-# Spriteset values
-export(Texture) var spriteset setget set_spriteset
-export(int) var hframes = 1 setget set_hframes
-export(int) var vframes = 1 setget set_vframes
-export(int) var frame = 0   setget set_frame
-export(int, "Mix", "Add", "Sub", "Mul", "PMAlpha") var blend_mode = 0 setget set_blend_mode
-
-# Instance members
-var Character = Sprite.new()
-
-########################
-### Export functions ###
-########################
-func set_spriteset(value):
-	spriteset = value
-	Character.set_texture(value)
-
-func set_hframes(value):
-	if value > 0:
-		hframes = value
-		Character.set_hframes(value)
-
-func set_vframes(value):
-	if value > 0:
-		vframes = value
-		Character.set_vframes(value)
-
-func set_frame(idx):
-	if 0 <= idx && idx < vframes * hframes:
-		frame = idx
-		Character.set_frame(idx)
-
-func set_blend_mode(value):
-	blend_mode = value
-	Character.set_blend_mode(value)
-
 ######################
 ### Core functions ###
 ######################
 func _enter_tree():
-	# Adding children
-	if !is_a_parent_of(Character):
-		add_child(Character)
-
 	# Setting up
 	set_pickable(true)
 	connect("body_enter", self, "_on_area_body_enter")
