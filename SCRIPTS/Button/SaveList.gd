e65d2f8c4e40011e25479469771fbb1336afdc3a
Keyaku <antonio.luis.sarmento@gmail.com>
2017-06-25 16:33:01 +0100

    SaveList: Customizable save slot information
    Changes:
    - Added ability to customize save slot text
    - Reformatted slot_data container
    - Code refactor

--- SCRIPTS/Button/SaveList.gd
+++ SCRIPTS/Button/SaveList.gd
@@ -41,6 +41,34 @@ func new_slot(cb_node, callback):
 	Slots.add_child(node)
 	return node
 
+func edit_slot(node, slot_data):
+	# If slot_data is null, create an empty slot_data
+	if slot_data == null:
+		slot_data = {
+			lv         = "",
+			playtime   = "",
+			location   = "",
+			difficulty = "",
+			img_world  = "",
+			img_avatar = ""
+		}
+
+	# Setting text labels
+	node.get_node("LV").set_text(slot_data.lv)
+	node.get_node("Difficulty").set_text(slot_data.difficulty)
+	node.get_node("Location").set_text(slot_data.location)
+	node.get_node("Playtime").set_text(slot_data.playtime)
+
+	# Setting images
+	var img = File.new()
+	var path_world = PATH_IMG_WORLD + slot_data.img_world + ".png"
+	if img.file_exists(path_world):
+		node.get_node("World").set_texture(load(path_world))
+
+	var path_avatar = PATH_IMG_AVATAR + slot_data.img_avatar + ".png"
+	if img.file_exists(path_avatar):
+		node.get_node("Avatar").set_texture(load(path_avatar))
+
 func fetch_saves(cb_node, callback):
 	for filename in SaveManager.get_save_list():
 		var slot_idx = int(filename)
@@ -52,26 +80,19 @@ func fetch_saves(cb_node, callback):
 		node.set_name(filename)
 
 		# Populating button with information
-		var hrs  = String(data.playtime_hrs if data.playtime_hrs != null else 0).pad_zeros(2)
-		var mins = String(data.playtime_min if data.playtime_min != null else 0).pad_zeros(2)
-		var location   = data.location   if data.location != null   else "null"
-		var difficulty = data.difficulty if data.difficulty != null else "null"
-		var location   = data.location   if data.location != null   else "null"
+		var hrs    = String(data.playtime_hrs if data.playtime_hrs != null else 0).pad_zeros(2)
+		var mins   = String(data.playtime_min if data.playtime_min != null else 0).pad_zeros(2)
 
-		if data.world != null && data.avatar != null:
-			var img_world = PATH_IMG_WORLD + data.world.to_lower() + ".png"
-			var img_avatar = PATH_IMG_AVATAR + data.avatar.to_lower() + ".png"
+		var slot_data = {
+			lv         = tr("LEVEL") + "." + String(data.lv if data.lv != null else 0),
+			playtime   = hrs + ":" + mins,
+			location   = data.location   if data.location != null   else "null",
+			difficulty = data.difficulty if data.difficulty != null else "null",
+			img_world  = data.world.to_lower()  if data.world != null  else "",
+			img_avatar = data.avatar.to_lower() if data.avatar != null else "",
+		}
 
-			var img = File.new()
-			if img.file_exists(img_world):
-				node.get_node("World").set_texture(load(img_world))
-			if img.file_exists(img_avatar):
-				node.get_node("Avatar").set_texture(load(img_avatar))
-
-		node.get_node("LV").set_text(tr("LEVEL") + "." + String(data.lv))
-		node.get_node("Difficulty").set_text(difficulty)
-		node.get_node("Location").set_text(location)
-		node.get_node("Playtime").set_text(hrs + ":" + mins)
+		edit_slot(node, slot_data)
 
 	emit_signal("finished")
 
