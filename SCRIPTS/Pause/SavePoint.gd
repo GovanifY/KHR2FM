a87dd4e1f8a59466209998fa3823f377d5ced008
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-03 16:03:26 +0100

    SavePoint: Corrected Show/Hide behavior
    Changes:
    - Removed input processing (it was dangerous as it was)
    - Removed unnecessary signal connection
    - Enabled can_cancel option on SavePoint’s buttons so they can dismiss
    it

--- SCRIPTS/Pause/SavePoint.gd
+++ SCRIPTS/Pause/SavePoint.gd
@@ -15,40 +15,32 @@ var cursor_idx = 0
 ### Core functions ###
 ######################
 func _ready():
-	connect("draw", self, "_show")
-
 	# Connecting main options
 	for i in range(0, Options.size()):
 		Options[i].connect("pressed", self, "_pressed", [i])
+		Options[i].connect("cancel", self, "_hide")
 		if i < Menu.size(): # FIXME: temp code
 			Menu[i].connect("hide", self, "_dismissed_menu")
-
-	set_process_input(true)
-
-func _input(event):
-	# TODO: Fix when holding cancel form menu
-	# and going back : it closes
-	if event.is_action("ui_cancel"):
-		queue_free()
 
 func _dismissed_menu():
 	for i in range(0, Options.size()):
 		Options[i].set_focus_mode(FOCUS_ALL)
 	Options[cursor_idx].grab_focus()
-	set_process_input(true)
 
 #######################
 ### Signal routines ###
 #######################
-func _show():
+func _draw():
+	# TODO: animate scene before fully showing
 	KHR2.pause_game(true)
 	Options[0].grab_focus()
 
 func _hide():
+	# TODO: animate scene before freeing
 	KHR2.pause_game(false)
+	queue_free()
 
 func _pressed(button_idx):
-	set_process_input(false)
 	cursor_idx = button_idx
 
 	if button_idx >= Menu.size():
