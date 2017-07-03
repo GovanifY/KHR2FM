7ae5d4fa4ba54ef67fd724f3eb9fd98a0da52b5a
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-03 01:50:19 +0100

    InfoBar: Removed unnecessary onready

--- SCRIPTS/InfoBar.gd
+++ SCRIPTS/InfoBar.gd
@@ -7,16 +7,16 @@ signal dismiss
 export(String, MULTILINE) var info_message = "INFO_BATTLE_MESSAGE"
 export(bool) var autostart = true
 
-onready var displayed_text = ""
-onready var displayed_pos = 0
+var displayed_text = ""
+var displayed_pos = 0
 
 ## True: Scrolling to the right
 ## False: Scrolling to the left
-onready var display_direction = true
+var display_direction = true
 
-onready var accumulator = 0
+var accumulator = 0
 
-onready var is_changing = false
+var is_changing = false
 
 # Instance members
 onready var Slide      = get_node("Slide")
@@ -38,14 +38,14 @@ func _input(event):
 	if event.is_pressed() && !event.is_echo():
 		if event.is_action("ui_accept"):
 			TextScroll.confirm()
-			
+
 func _fixed_process(delta):
 	accumulator+=delta
 	if accumulator > 1:
-		if is_changing: 
+		if is_changing:
 			accumulator = 0
 			is_changing = false
-			
+
 		if accumulator > 1.05 && accumulator != 0:
 			if display_direction == true && (displayed_pos+30)<(info_message.length()+1):
 				displayed_pos+=1
@@ -60,6 +60,7 @@ func _fixed_process(delta):
 			else:
 				display_direction = !display_direction
 				is_changing = true
+
 #######################
 ### Signal routines ###
 #######################
@@ -77,7 +78,7 @@ func _start_input():
 	set_process_input(true)
 	if info_message.length() > 30:
 		set_fixed_process(true)
-	
+
 func _start_dismissal():
 	set_process_input(false)
 	Slide.connect("finished", self, "_end_dismissal")
