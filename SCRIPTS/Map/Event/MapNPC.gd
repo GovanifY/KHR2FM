2255e8b2da8459d07864e5a10760962c20212221
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-04 12:16:41 +0100

    MapNPC: Added ability to speak with an NPC
    Changes:
    - Instancing and freeing a Dialogue node when speaking to an NPC
    - Exported a StringArray for the NPC’s lines
    - Automagically iterating through all the lines like magical magic

--- SCRIPTS/Map/Event/MapNPC.gd
+++ SCRIPTS/Map/Event/MapNPC.gd
@@ -1,5 +1,25 @@
 extends "MapEvent.gd"
 
+# ID lines (or text)
+export(StringArray) var lines
+
+# Member variables
+var path_dialogue = "res://SCENES/Dialogue/Dialogue.tscn"
+
 func _interacted():
-	# FIXME: Fill with stuff
-	print("sup man")
+	if lines == null:
+		return
+
+	if Globals.get("Dialogue") == null:
+		SceneLoader.load_scene(path_dialogue, SceneLoader.BACKGROUND)
+		var Dialogue = SceneLoader.show_scene(path_dialogue)
+		Dialogue.connect("hide", SceneLoader, "erase_scene", [Dialogue])
+
+		# Writing NPC's lines
+		Dialogue.show()
+
+		for line in lines:
+			Dialogue.write(line)
+			yield(Dialogue, "finished")
+
+		Dialogue.hide()
