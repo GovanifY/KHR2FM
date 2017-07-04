69502b897057945a38318104240be475befbec5f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-04 09:36:55 +0100

    MapSave: Loading at regular priority

--- SCRIPTS/Map/Event/MapSave.gd
+++ SCRIPTS/Map/Event/MapSave.gd
@@ -3,6 +3,6 @@ extends "MapEvent.gd"
 var path_save = "res://SCENES/Pause/SaveMenu.tscn"
 
 func _interacted():
-	SceneLoader.load_scene(path_save, SceneLoader.BACKGROUND | SceneLoader.HIGH_PRIORITY)
+	SceneLoader.load_scene(path_save, SceneLoader.BACKGROUND)
 	var scene = SceneLoader.show_scene(path_save)
 	scene.get_node("Panel").connect("hide", SceneLoader, "erase_scene", [scene])
