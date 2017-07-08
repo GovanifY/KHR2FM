d192b667e88464c5c6188116a2577de46123aa9d
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-08 02:17:33 +0100

    MapSave: Bug fix

--- SCRIPTS/Map/Event/MapSave.gd
+++ SCRIPTS/Map/Event/MapSave.gd
@@ -5,4 +5,4 @@ var path_save = "res://SCENES/Pause/SavePoint.tscn"
 func _interacted():
 	SceneLoader.load_scene(path_save, SceneLoader.BACKGROUND)
 	var scene = SceneLoader.show_scene(path_save)
-	scene.get_child(0).connect("hide", SceneLoader, "erase_scene", [scene])
+	scene.connect("hide", SceneLoader, "erase_scene", [scene])
