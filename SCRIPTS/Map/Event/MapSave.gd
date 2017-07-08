6f0c9c9eb11b9b6a45ae19662ba4763c2891e330
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-08 02:48:21 +0100

    MapSave: Properly displaying SavePoint menu

--- SCRIPTS/Map/Event/MapSave.gd
+++ SCRIPTS/Map/Event/MapSave.gd
@@ -4,7 +4,9 @@ onready var path_pause = KHR2.get_pause()
 
 func _interacted():
 	KHR2.set_pause(load("res://SCENES/Pause/SavePoint.tscn"))
-	KHR2.get_node("Pause").connect("hide", self, "_dismissed")
+	var pause = KHR2.get_node("Pause")
+	pause.show()
+	pause.connect("hide", self, "_dismissed")
 
 func _dismissed():
 	KHR2.set_pause(load(path_pause))
