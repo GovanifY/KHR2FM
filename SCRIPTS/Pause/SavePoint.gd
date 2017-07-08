6be57338e9058e8740b1f363cbd4c1ccb040b312
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-08 22:41:58 +0100

    SavePoint: Set up Confirmation "window"
    Changes:
    - Added “Are you sure?” translation line
    - Prepared Confirm window in SavePoint
    - Wrote minimal code for firing up World Map

--- SCRIPTS/Pause/SavePoint.gd
+++ SCRIPTS/Pause/SavePoint.gd
@@ -5,8 +5,10 @@ enum { SAVEPOINT_SAVE, SAVEPOINT_WORLD }
 
 # Instance members
 onready var Options = get_node("Options").get_children()
+onready var Choices = get_node("Confirm/BinaryChoice")
 onready var Menu    = [
 	get_node("SaveMenu"),
+	get_node("Confirm"),
 ]
 
 var cursor_idx = 0
@@ -21,26 +23,26 @@ func _ready():
 	for i in range(0, Options.size()):
 		Options[i].connect("pressed", self, "_pressed", [i])
 		Options[i].connect("cancel", self, "hide")
-		if i < Menu.size(): # FIXME: temp code until "World Map" button is done
-			Menu[i].connect("hide", self, "_dismissed_menu")
+
+		Menu[i].connect("hide", self, "_dismissed_menu")
+		Menu[i].hide()
+
+	# Setting up Choices
+	Choices.connect("draw", Choices.no, "grab_focus")
+	Choices.connect("pressed", self, "confirm_choice") # FIXME: load World Map
 
 #######################
 ### Signal routines ###
 #######################
 func _draw():
-	# TODO: animate scene before fully showing
 	KHR2.pause_game(true)
 	Options[0].grab_focus()
 
 func _hide():
-	# TODO: animate scene before freeing
 	KHR2.pause_game(false)
 
 func _pressed(button_idx):
 	cursor_idx = button_idx
-
-	if button_idx >= Menu.size():
-		return # FIXME: temp code until "World Map" button is done
 
 	# General rules
 	for i in range(0, Options.size()):
@@ -53,3 +55,15 @@ func _dismissed_menu():
 	for i in range(0, Options.size()):
 		Options[i].set_focus_mode(Options[i].FOCUS_ALL)
 	Options[cursor_idx].grab_focus()
+
+###############
+### Methods ###
+###############
+func confirm_choice(yes):
+	if not yes:
+		Menu[cursor_idx].anims.play("Fade Out")
+		return false
+
+	if cursor_idx == SAVEPOINT_WORLD:
+		# TODO: Load World Map
+		Menu[cursor_idx].anims.play("Fade Out")
