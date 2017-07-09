14260095b877300576e480564a3f8b2492e79baf
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-09 19:38:08 +0100

    InfoBar: Minor improvements
    Changes:
    - Went from fixed to idle processing
    - Added constants to be easier to read
    - Disabled processing upon dismissal
    - Code refactor

--- SCRIPTS/InfoBar.gd
+++ SCRIPTS/InfoBar.gd
@@ -13,12 +13,11 @@ onready var Slide      = get_node("Slide")
 onready var TextScroll = get_node("InfoLabel/TextScroll")
 
 # Private members
-var displayed_text = ""
-var displayed_pos = 0
+const MAX_VISIBLE_CHARS = 30
 
-## True: Scrolling to the right
-## False: Scrolling to the left
-var display_direction = true
+enum { DIR_LEFT, DIR_RIGHT }
+var display_direction = DIR_RIGHT
+var displayed_pos = 0
 
 var accumulator = 0
 
@@ -38,24 +37,21 @@ func _input(event):
 		if event.is_action("ui_accept"):
 			TextScroll.confirm()
 
-func _fixed_process(delta):
+func _process(delta):
 	accumulator+=delta
 	if accumulator > 1:
 		if is_changing:
 			accumulator = 0
 			is_changing = false
 
-		if accumulator > 1.05 && accumulator != 0:
-			if display_direction == true && (displayed_pos+30)<(info_message.length()+1):
+		if accumulator > 1.05:
+			TextScroll.set_text_raw(info_message.substr(displayed_pos, displayed_pos+MAX_VISIBLE_CHARS))
+			if display_direction == DIR_RIGHT && (displayed_pos+MAX_VISIBLE_CHARS)<(info_message.length()+1):
 				displayed_pos+=1
 				accumulator = 1
-				TextScroll.set_text_raw(info_message.substr(displayed_pos, displayed_pos+30))
-				TextScroll.set_visibility_raw(30)
-			elif display_direction == false && (displayed_pos+1>1):
+			elif display_direction == DIR_LEFT && (displayed_pos+1>1):
 				displayed_pos-=1
 				accumulator = 1
-				TextScroll.set_text_raw(info_message.substr(displayed_pos, displayed_pos+30))
-				TextScroll.set_visibility_raw(30)
 			else:
 				display_direction = !display_direction
 				is_changing = true
@@ -74,21 +70,22 @@ func play():
 
 	# Start scrolling text. Partially if it's too long
 	if info_message.length() > 30:
-		displayed_text = info_message.substr(0, 30)
 		displayed_pos=0
-		TextScroll.scroll(displayed_text)
+		TextScroll.scroll(info_message.substr(0, MAX_VISIBLE_CHARS))
+		TextScroll.set_visibility_raw(MAX_VISIBLE_CHARS)
 	else:
 		TextScroll.scroll(info_message)
 	yield(TextScroll, "finished")
 
 	# Accept input to dismiss info bar
 	set_process_input(true)
-	if info_message.length() > 30:
-		set_fixed_process(true)
+	if info_message.length() > MAX_VISIBLE_CHARS:
+		set_process(true)
 	yield(TextScroll, "cleared")
 
 	# Dismiss info bar
 	set_process_input(false)
+	set_process(false)
 	Slide.play("Out")
 	yield(Slide, "finished")
 	emit_signal("dismiss")
