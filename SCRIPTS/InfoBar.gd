e28ee1c168de90d590bf19e578492a1387e493a6
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-10 19:37:11 +0100

    InfoBar: Better text slide animation
    Removed "hacky" code in favor of a combination of ScrollBars and
    Tweening.

--- SCRIPTS/InfoBar.gd
+++ SCRIPTS/InfoBar.gd
@@ -10,26 +10,25 @@ export(bool) var autostart = true
 
 # Instance members
 onready var Slide      = get_node("Slide")
-onready var Info       = get_node("InfoLabel")
-onready var TextScroll = get_node("InfoLabel/TextScroll")
-onready var Overflow   = get_node("InfoLabel/Overflow")
+onready var Constraint = get_node("Constraint")
 
-onready var info_font = Info.get("custom_fonts/font")
+onready var Info       = get_node("Constraint/InfoLabel")
+onready var TextScroll = Info.get_node("TextScroll")
+onready var Overflow   = Info.get_node("Overflow")
+onready var Delay      = Info.get_node("Delay")
 
 # Private members
-enum { LEFT, RIGHT }
-var display_direction = RIGHT
-var displayed_pos = 0
-
-const END_DELAY = 1.0
-var wait_time = 0
-
+const DURATION = 1.0 # FIXME: Shouldn't be a fixed value
 
 ######################
 ### Core functions ###
 ######################
 func _ready():
-	Overflow.connect("timeout", self, "_overflow")
+	# Setting text slide animation
+	Constraint.set_h_scroll(0)
+	Overflow.connect("tween_complete", self, "_on_Overflow_end")
+
+	# Firing it up
 	if autostart:
 		call_deferred("play")
 
@@ -39,23 +38,24 @@ func _input(event):
 		if event.is_action("ui_accept"):
 			TextScroll.confirm()
 
-func _overflow():
-	# Delays function each time the text reaches each side
-	if wait_time < END_DELAY:
-		wait_time += Overflow.get_wait_time()
-		return
+func _on_Overflow_end(_, _):
+	Delay.start()
+	yield(Delay, "timeout")
 
-	var displayed_text = info_message.substr(displayed_pos, info_message.length())
-	var length = info_font.get_string_size(displayed_text).width
-	TextScroll.set_text_raw(displayed_text)
-
-	if display_direction == RIGHT && length >= Info.get_size().width:
-		displayed_pos += 1
-	elif display_direction == LEFT && displayed_text != info_message:
-		displayed_pos -= 1
+	# If it's at the far left, start sliding right
+	if Constraint.get_h_scroll() == 0:
+		var length = Info.get_size().width - Constraint.get_size().width
+		Overflow.interpolate_method(
+			Constraint, "set_h_scroll", 0, length, DURATION,
+			Overflow.TRANS_LINEAR, Overflow.EASE_IN
+		)
 	else:
-		display_direction = LEFT if display_direction == RIGHT else RIGHT
-		wait_time = 0
+		Overflow.interpolate_method(
+			Constraint, "set_h_scroll", Constraint.get_h_scroll(), 0, DURATION,
+			Overflow.TRANS_LINEAR, Overflow.EASE_IN
+		)
+
+	Overflow.call_deferred("start")
 
 ###############
 ### Methods ###
@@ -64,8 +64,6 @@ func set_text(text):
 	info_message = text
 
 func play():
-	var length = info_font.get_string_size(info_message).width
-
 	# Display info bar
 	Slide.play("In")
 	yield(Slide, "finished")
@@ -77,15 +75,13 @@ func play():
 
 	# Accept input to dismiss info bar
 	set_process_input(true)
-	if length >= Info.get_size().width:
-		displayed_pos = 0
-		wait_time = 0
-		Overflow.start()
+	if Info.get_size().width >= Constraint.get_size().width:
+		_on_Overflow_end(null, null)
 	yield(TextScroll, "cleared")
 
 	# Dismiss info bar
 	set_process_input(false)
-	Overflow.stop()
+	Overflow.stop_all()
 	Slide.play("Out")
 	yield(Slide, "finished")
 	emit_signal("dismiss")
