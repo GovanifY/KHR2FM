f686970d71710e6379f2a5c481eb6dff8f3413a7
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-11 23:09:03 +0100

    MapEnemy: Saving current scene's filename for later

--- SCRIPTS/Map/Event/MapEnemy.gd
+++ SCRIPTS/Map/Event/MapEnemy.gd
@@ -5,4 +5,6 @@ export(String, FILE, "tscn") var battle_scene
 func _player_touched():
 	# TODO: Save exact coordinates of this map before switching scenes
 	# TODO: Play introductory animation (the heart explosion thing)
-	SceneLoader.load_scene(battle_scene)
+	SceneLoader.queue_scene(battle_scene)
+	SceneLoader.queue_scene(get_tree().get_current_scene().get_filename())
+	SceneLoader.load_next_scene()
