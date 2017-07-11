668c9b6c5dcdda85f16eb3dcdf6c85e87cf29b2f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-11 23:16:37 +0100

    SaveManager: Reorganized Save scripts

--- SCRIPTS/AutoLoad/SaveManager.gd
+++ SCRIPTS/AutoLoad/SaveManager.gd
@@ -13,7 +13,7 @@ const SAVE_NAME = "slot"
 const SAVE_EXT  = "save"
 
 # SaveSlot class and current slot object
-const SaveSlot = preload("res://SCRIPTS/SaveSlot.gd")
+const SaveSlot = preload("SaveManager/SaveSlot.gd")
 var current_slot
 
 ######################
