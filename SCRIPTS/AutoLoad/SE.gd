6c2e9d299bed894cfa79ac421389d8ecf4399605
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-11 23:34:15 +0100

    SE: Saving default volume to ConfigFile

---
+++ SCRIPTS/AutoLoad/SE.gd
@@ -0,0 +1,16 @@
+extends SamplePlayer
+
+
+######################
+### Core functions ###
+######################
+func _ready():
+	# Grabbing saved volume
+	if !KHR2.config.has_section_key("sound", "volume"):
+		set_default_volume(get_default_volume())
+	else:
+		set_default_volume(KHR2.config.get_section_key("sound", "volume"))
+
+func set_default_volume(value):
+	.set_default_volume(value)
+	KHR2.config.set_value("sound", "volume", value)
