de3b9d5f252e25d6a9019c9f8900a568f3146eee
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-12 18:10:08 +0100

    MapEnemy: Added spawning animation
    Changes:
    - Added Heartless spawn animation
    - Playing animation when instanced and freed
    - Abstracted MapHeartless from Shadow aspects

--- SCRIPTS/Map/Event/MapEnemy.gd
+++ SCRIPTS/Map/Event/MapEnemy.gd
@@ -1,6 +1,14 @@
 extends "MapEvent.gd"
 
+# Battle scene to load upon touching
 export(String, FILE, "tscn") var battle_scene
+
+# Instance members
+onready var spawn = get_node("spawn/anim")
+
+func _ready():
+	if "In" in spawn.get_animation_list():
+		spawn.play("In")
 
 func _player_touched():
 	# TODO: Save exact coordinates of this map before switching scenes
@@ -9,3 +17,9 @@ func _player_touched():
 		SceneLoader.queue_scene(battle_scene)
 		SceneLoader.queue_scene(get_tree().get_current_scene().get_filename())
 		SceneLoader.load_next_scene()
+
+func queue_free():
+	if "Out" in spawn.get_animation_list():
+		spawn.play("Out")
+		yield(spawn, "finished")
+	.queue_free()
