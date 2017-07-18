13069a4cec6e75362b9b033dc493f27072daf928
Gauvain Roussel-Tarbouriech <govanify@protonmail.com>
2017-07-18 03:04:33 +0200

    New intro redone as I could

--- GAME/STORY/Intro/Intro.gd
+++ GAME/STORY/Intro/Intro.gd
@@ -3,6 +3,7 @@ extends Node
 # Instance members
 onready var Dialogue = get_node("Dialogue")
 onready var Sequences = get_node("Master")
+onready var Loop = get_node("Loop")
 
 # Characters
 onready var Kiryoku  = Dialogue.get_node("Kiryoku")
@@ -37,28 +38,44 @@ func _ready():
 	]:
 		line[0].speak(line[1], line[2])
 		yield(line[0], "finished")
-
+	
+	Sequences.play("Hurt")
+	yield(Sequences, "finished")
+	
 	Dialogue.hide()
 	
 	Sequences.play("Flash")
 	yield(Sequences, "finished")
 	
-	Sequences.play("Kiryoku vanish")
+	
+	
+	Loop.play("Hand")
+	
+	Sequences.play("Hand")
 	yield(Sequences, "finished")
+	
 
-	# Change music
-	Music.set_stream(preload("res://ASSETS/BGM/The Eye of Darkness.ogg"))
-	Music.play()
+
 
 	for line in [
-		[Kiryoku, 4, 10],
-		[Yuugure, 7, 8],
+		[Kiryoku, 4, 10]
+	]:
+		line[0].speak(line[1], line[2])
+		yield(line[0], "finished")
+		
+	Dialogue.hide()
+	
+	Sequences.play("Keyblade")
+	yield(Sequences, "finished")
+	
+	for line in [
+	[	Yuugure, 7, 8],
 		[Kiryoku, 11, 16],
 		[Yuugure, 9, 12],
 		[Kiryoku, 17, 19],
 	]:
 		line[0].speak(line[1], line[2])
 		yield(line[0], "finished")
-
+	
 	# Load next scene
 	SceneLoader.load_next_scene()
