5c2070ce7bccf4d933840a5e9a839b0f668b76ed
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-25 01:31:15 +0100

    Stats: Updated tests with BattlerStats

--- GAME/DebugRoom/RPG/StatsTest.gd
+++ GAME/DebugRoom/RPG/StatsTest.gd
@@ -2,9 +2,11 @@ extends Node
 
 const Modifier  = preload("res://SCRIPTS/RPG/Stats/Modifier.gd")
 const Stats     = preload("res://SCRIPTS/RPG/Stats/Stats.gd")
+const BattlerStats = preload("res://SCRIPTS/RPG/Stats/BattlerStats.gd")
 
+# "Weapons"
 onready var kingdom_key = {
-	"id"  : 1234,
+	"id"  : 1,
 	"mod" : Modifier.new({
 		"add" : {
 			"str" : 1,
@@ -12,8 +14,10 @@ onready var kingdom_key = {
 		}
 	})
 }
+
+# Status effects
 onready var bravery = {
-	"id"  : 5,
+	"id"  : 2,
 	"mod" : Modifier.new({
 		"mul" : {
 			"str" : 0.1,
@@ -21,7 +25,25 @@ onready var bravery = {
 		}
 	})
 }
+onready var curse = {
+	"id"  : 3,
+	"mod" : Modifier.new({
+		"mul" : {
+			"max_hp" : -0.5,
+		}
+	})
+}
+
+# Character (basic stats)
 onready var character = Stats.new({
+	"str" : 7,
+	"def" : 3,
+})
+
+# Battler
+onready var battler = BattlerStats.new({
+	"max_hp" : 20, "max_mp" : 10,
+	"hp"     : 20, "mp"     : 10,
 	"str" : 7,
 	"def" : 3,
 })
@@ -30,8 +52,18 @@ func _ready():
 	pass
 	#test_one_stat()
 	#test_equipment()
-	test_status()
+	#test_status()
+	test_battle()
 
+# Function Helpers
+static func apply_mod(target, value):
+	target.add_modifier(value.id, value.mod)
+	target.print_stats()
+	
+static func reset_mods(target):
+	target.modifiers.clear()
+
+####### TESTERS #######
 func test_one_stat():
 	character.print_stat("str")
 
@@ -56,7 +88,7 @@ func test_status():
 	print("'Bravery' consists in:")
 	bravery.mod.print_stats()
 
-	print("\nYour character gained 'Bravery'")
+	print("\nYour character got 'Bravery'")
 	character.add_modifier(bravery.id, bravery.mod)
 	character.print_stat("str")
 	character.print_stat("def")
@@ -65,3 +97,17 @@ func test_status():
 	character.remove_modifier(bravery.id)
 	character.print_stat("str")
 	character.print_stat("def")
+	
+	reset_mods(character)
+	
+func test_battle():
+	print("Initial stats")
+	battler.print_stats()
+
+	print("\nYour battler got 'Bravery'")
+	apply_mod(battler, bravery)
+	
+	print("\nYour battler got 'Curse'")
+	apply_mod(battler, curse)
+	
+	reset_mods(battler)
