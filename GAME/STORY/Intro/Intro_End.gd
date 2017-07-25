e83c97b20130710f03237616cc23bf8e02a1ef69
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-25 03:12:21 +0100

    Intro_end fixes
    Changes:
    - Translated lines to English
    - Removed clutter
    - Refactored code

--- GAME/STORY/Intro/Intro_End.gd
+++ GAME/STORY/Intro/Intro_End.gd
@@ -2,65 +2,37 @@ extends Node
 
 # Instance members
 onready var Sequences = get_node("Master")
-onready var Dialogue = get_node("Dialogue")
-onready var Kiryoku  = Dialogue.get_node("Kiryoku")
-onready var InfoBar = get_node("Escape/InfoBar")
+onready var Dialogue  = get_node("Dialogue")
+onready var InfoBar   = get_node("Escape/InfoBar")
 
+# Characters
+onready var Kiryoku  = Dialogue.get_node("Kiryoku")
 
 ######################
 ### Core functions ###
 ######################
 func _ready():
+	# Prepare BGM
+	Music.set_stream(preload("res://ASSETS/BGM/Don't give up.ogg"))
+	Music.play()
+
+	# Prepare Dialogue for this particular scene
+	Dialogue.set_box(0)
+
 	# Begin cutscene
 	Sequences.play("Kiryoku_down")
 	yield(Sequences, "finished")
 
-	Dialogue.set_box(2)
-	Dialogue.show(true)
-	yield(Dialogue.Bubble, "shown")
-
-	for line in [
-		[Kiryoku, 20, 21]
-	]:
-		line[0].speak(line[1], line[2])
-		yield(Dialogue, "finished")
+	Kiryoku.speak(20, 21)
+	yield(Kiryoku, "finished")
 
 	Sequences.play("Swap_Escape")
 	yield(Sequences, "finished")
-	
-	#I made a dummy animation to be able to hide dialogue before showing it again
-	#Dialogue.show()
-	
-	#Dialogue.show()
-	for line in [
-		[Kiryoku, 22, 23]
-	]:
-		line[0].speak(line[1], line[2])
-		yield(Dialogue, "finished")
-	
+
+	Kiryoku.speak(22, 23)
+	yield(Kiryoku, "finished")
+
 	Dialogue.hide()
-
 	InfoBar.play()
-	
-	
-	#ANIM: to black then fade in to battle screen then dialogue appears
-	#Dialogue.hide()
 
-#	Sequences.play("Water")
-#	yield(Sequences, "finished")
-
-#	get_node("Kiryoku_down").play()
-#	Sequences.play("Kiryoku down")
-#	yield(Sequences, "finished")
-
-	#for line in [
-	#	[Yuugure, 0, 4],
-	#	[Kiryoku, 0, 3],
-	#	[Yuugure, 5, 6],
-	#]:
-	#	line[0].speak(line[1], line[2])
-	#	yield(line[0], "finished")
-	
-	# Load next scene
-	#SceneLoader.transition("Battle")
-	#SceneLoader.load_next_scene()
+	# TODO: to black then fade in to battle screen then dialogue appears
