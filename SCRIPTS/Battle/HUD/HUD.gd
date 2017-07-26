4ab75e32656cbd1718a6b8d2d4f5b096818f8ef8
Keyaku <antonio.luis.sarmento@gmail.com>
2017-07-26 03:32:14 +0100

    Battle HUD: Updating current & max value in a Label

--- SCRIPTS/Battle/HUD/HUD.gd
+++ SCRIPTS/Battle/HUD/HUD.gd
@@ -15,6 +15,13 @@ func _ready():
 	reset_hud(get_node("Player"), player)
 	reset_hud(get_node("Enemy"), enemy)
 
+# Updates the label of a given ProgressBar node, if available
+func _update_label(node, value):
+	if node.has_node("Value"):
+		var label = node.get_node("Value")
+		if label.is_visible():
+			label.set_text("%d / %d" % [value, node.get_max()])
+
 ###############
 ### Methods ###
 ###############
@@ -37,7 +44,16 @@ func set_stats(member, stats):
 	for key in member:
 		# If it exists in stats, set its value
 		if key in stats.final:
-			member[key].set_value(stats.get(key))
+			var value = stats.get(key)
+
+			# Set the maximum value
+			var max_stat = "max_" + key
+			if max_stat in stats.final:
+				member[key].set_max(value)
+			member[key].set_value(value)
+
+			# Set the label node
+			_update_label(member[key], value)
 
 # Sets given stat values for the player
 func set_player_stats(stats):
@@ -51,3 +67,4 @@ func set_enemy_stats(stats):
 # e.g. player["hp"].set_value(50)
 func apply_value(key, value, member):
 	member[key].set_value(value)
+	_update_label(member[key], value)
