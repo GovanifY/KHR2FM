82849ba3d31154f1a0ab4f644c5cabb297e70c91
Keyaku <antonio.luis.sarmento@gmail.com>
2017-08-02 02:24:36 +0100

    RoundProgress: Better data manipulation
    Changes:
    - Brought the polygons to Polygon2D nodes
    - Undifferentiated the arc angle (it’s always going to be 270º, at
    least for now)
    - Code refactor

--- SCRIPTS/RoundProgress.gd
+++ SCRIPTS/RoundProgress.gd
@@ -3,12 +3,16 @@ extends Range
 
 # Export values
 export(int) var thickness = 20 setget set_thickness
-
 export(Color, RGB) var color = Color(0, 1, 0) setget set_color
 
 # Constants
 const BG_COLOR = Color(0.2, 0.2, 0.2)
+const ARC_ANGLE = 270
 const MAX_ARC_VALUE = 100
+
+# Instance members
+var bg # Background Polygon2D
+var fg # Foreground Polygon2D
 
 # "Private" members
 var half_thickness
@@ -30,23 +34,41 @@ func set_thickness(value):
 ######################
 ### Core functions ###
 ######################
+func _ready():
+	# Setting bars
+	if bg == null:
+		bg = Polygon2D.new()
+		bg.set_color(BG_COLOR)
+		bg.set_use_parent_material(true)
+		add_child(bg)
+
+	if fg == null:
+		fg = Polygon2D.new()
+		fg.set_color(color)
+		fg.set_use_parent_material(true)
+		add_child(fg)
+
 # Overloading functions
 func _draw():
-	# Let us draw this whole thing from scratch
 	var radius = (int(min(get_size().x, get_size().y)) >> 1) - thickness
 	var center = get_size() * 0.5
 
 	# Draw background, then progress
-	_draw_round_bar(center, radius, BG_COLOR, get_max())
-	_draw_round_bar(center, radius, color, get_value())
+	var points_bg = get_progress_bar(center, radius, get_max())
+	var points_fg = get_progress_bar(center, radius, get_value())
+	if points_bg != null:
+		bg.set_polygon(points_bg)
 
-# Draws our bar
-func _draw_round_bar(center, radius, color, value):
+	if points_fg != null:
+		fg.set_polygon(points_fg)
+
+# Gets complete polygon for our bar
+func get_progress_bar(center, radius, value):
 	var arc_amount = max(min(value, MAX_ARC_VALUE), 0)
-	var points = get_circle_arc(center, radius, 270, arc_amount)
+	var points = get_circle_arc(center, radius, arc_amount)
 
 	if value > MAX_ARC_VALUE:
-		var rect_pos = get_rect_bar_position(center, radius-half_thickness, 270)
+		var rect_pos = get_rect_bar_position(center, radius-half_thickness)
 		var rest_value = value - MAX_ARC_VALUE if value >= MAX_ARC_VALUE else 0
 
 		rect_pos.x -= rest_value
@@ -54,24 +76,21 @@ func _draw_round_bar(center, radius, color, value):
 		rect_pos.y += thickness
 		points.insert(33, rect_pos)
 
-	if points != null:
-		draw_colored_polygon(points, color)
+	return points
 
 ########################
 ### Helper functions ###
 ########################
-func get_rect_bar_position(center, radius, maximum):
-	maximum = max(min(maximum, 360), 0)
+func get_rect_bar_position(center, radius):
 	var angle_from = -180
-	var angle_to = maximum + angle_from
+	var angle_to = ARC_ANGLE + angle_from
 
 	var position = center + Vector2( cos(deg2rad(angle_to)), sin(deg2rad(angle_to)) ) * radius
 	return position
 
-func get_circle_arc(center, radius, maximum, amount):
-	maximum = max(min(maximum, 360), 0)
+func get_circle_arc(center, radius, amount):
 	var angle_from = -180
-	var angle_to = maximum * amount / MAX_ARC_VALUE + angle_from
+	var angle_to = ARC_ANGLE * amount / MAX_ARC_VALUE + angle_from
 
 	if angle_from >= angle_to:
 		return
